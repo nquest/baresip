3ff12d3d1170372df252457c86697936a7702f50
Sergey Moryakov <sergey@nqnet.org>
2022-11-26 15:07:49 +0530

    .

--- src/aites.c
+++ src/aites.c
@@ -823,7 +823,7 @@ void gpioCheckThread(deviceInfo *deviceConfig)
             break;
 
         printf("DEBUG: BEFORE UA_CALL");
-        *curr_call = ua_call(aites_call_s->uac);
+        curr_call = ua_call(aites_call_s->uac);
         if (!curr_call)
         {
             curr_call->state = 0;
